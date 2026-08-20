af655278ebe6038e80aff6adc46835768b686dc0
govindjoshi9 <joshigovind740@gmail.com>
2026-08-20 14:35:22 +0530

    feat: add data tables for inventory, items, and branches modules

--- components/modules/branches/components/BranchesTable.tsx
+++ components/modules/branches/components/BranchesTable.tsx
@@ -103,12 +103,9 @@ export function BranchesTable({ branches, isLoading, onEdit, onDeactivate }: Bra
               </TableCell>
               <TableCell className="text-right">
                 <DropdownMenu>
-                  {/* @ts-expect-error */}
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" className="h-8 w-8 p-0 opacity-0 group-hover:opacity-100 focus:opacity-100 transition-opacity">
+                  <DropdownMenuTrigger render={<Button variant="ghost" className="h-8 w-8 p-0 opacity-0 group-hover:opacity-100 focus:opacity-100 transition-opacity" />}>
                       <span className="sr-only">Open menu</span>
                       <MoreHorizontal className="h-4 w-4" />
-                    </Button>
                   </DropdownMenuTrigger>
                   <DropdownMenuContent align="end" className="w-[160px]">
                     <DropdownMenuItem onClick={() => onEdit(branch)}>
--- components/modules/inventory/components/InventoryTable.tsx
+++ components/modules/inventory/components/InventoryTable.tsx
@@ -95,11 +95,9 @@ export function InventoryTable({ data, onRefresh }: InventoryTableProps) {
                     </TableCell>
                     <TableCell>
                       <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                          <Button variant="ghost" className="h-8 w-8 p-0">
+                        <DropdownMenuTrigger render={<Button variant="ghost" className="h-8 w-8 p-0" />}>
                             <span className="sr-only">Open menu</span>
                             <MoreHorizontal className="h-4 w-4" />
-                          </Button>
                         </DropdownMenuTrigger>
                         <DropdownMenuContent align="end">
                           <DropdownMenuItem
